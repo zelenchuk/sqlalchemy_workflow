815d44ef4418ef3705b96da4c3c1f07e14a09193
Sergei <zelenchyks@gmail.com>
2022-01-20 22:50:50 +0200

    Edit app.py

--- app.py
+++ app.py
@@ -10,7 +10,9 @@
 from dotenv import dotenv_values
 config = dotenv_values(".env")
 
+
 import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 params = {
   'dbname': config['DB_NAME'],
@@ -20,17 +22,47 @@ params = {
   'port': config['DB_PORT'],
 }
 
-conn = psycopg2.connect(**params)
+# conn = psycopg2.connect(**params)
 
 
-cur = conn.cursor()
+# cur = conn.cursor()
 
-cur.execute("SELECT * FROM auth_user")
+# cur.execute("SELECT * FROM auth_user")
 
-# Retrieve query results
-records = cur.fetchall()
-# print(records)
+# # Retrieve query results
+# records = cur.fetchall()
+# # print(records)
 
-for service in records:
-	print(service)
-	print("="*50)
+# for service in records:
+# 	print(service)
+# 	print("="*50)
+
+
+# cur.close()
+
+
+# conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
+# cursor = conn.cursor()  # Создаем курсор для выполнения операций с базой данных
+# sql_create_database = cursor.execute('create database sqlalchemy_tuts')   # Создаем базу данных
+
+
+# # Закрываем соединение
+# cursor.close()
+# conn.close()
+
+
+from sqlalchemy import create_engine
+
+a = 'postgresql+psycopg2://'+ params['user'] + ':' + params['password'] +'@' + params['host'] + ':' +params['port'] + '/' + params['dbname']
+
+# engine = create_engine(a)
+
+
+
+# engine.connect()
+
+# print(engine)
+
+
+print(a)
